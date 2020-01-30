cffc5b7dbe14dd61bb6856276c2088ca6362ab2b
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-29 20:17:05 -0700

    Add GET endpoint for all specific brewery

--- beers/server.js
+++ beers/server.js
@@ -17,7 +17,23 @@ app.locals.title = 'Breweries';
 app.get('/api/v1/breweries', async (request, response) => {
   try {
     const breweries = await database('breweries').select();
-    response.status(200).json(breweries);
+    response.status(200).json({breweries});
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
+
+app.get('/api/v1/breweries/:id', async (request, response) => {
+  try {
+    const breweries = await database('breweries').where('id', request.params.id).select();
+      if (breweries.length) {
+        response.status(200).json(breweries[0]);
+        // i'm using [0] because i just want the single object to be the response
+      } else {
+        response.status(404).json({
+          error: `Could not find breweries with an id of ${request.params.id}`
+        })
+      }
   } catch (error) {
     response.status(500).json({ error });
   }
