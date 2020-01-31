9db4c5c6f76ee4f9fc38a7e3dbec595aa655e577
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-31 09:42:16 -0700

    Add production environment

--- beers/knexfile.js
+++ beers/knexfile.js
@@ -12,39 +12,14 @@ module.exports = {
       directory: './db/seeds'
     },
     useNullAsDefault: true
-  }
-  // ,
-  //
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  },
 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 };
