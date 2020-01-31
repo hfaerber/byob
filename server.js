77251aeebf2941b13ddfd9c574da589901871806
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-31 09:22:02 -0700

    Move server file to root

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
+const configuration = require('./beers/knexfile')[environment];
 const database = require('knex')(configuration);
 
 const express = require('express');
