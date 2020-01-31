cf8f077b2ac481774fe257f95790fbaf3b4e3d89
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-31 10:08:08 -0700

    Troubleshoot heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./beers/knexfile.js')[environment];
+const configuration = require('./knexfile.js')[environment];
 const database = require('knex')(configuration);
 
 const express = require('express');
